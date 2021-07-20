697f0b031e47c97b82f03e58cf0b07828a39650e
Casino_Wolf <snowboardsilver7@gmail.com>
2021-07-19 23:50:37 -0700

    Initial

---
+++ PowerPC Assembly Functions/MainCode.cpp
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+#include "ReplayFix.h"
+#include "Controls.h"
+#include "StopStartAlts.h"
+#include "Save States.h"
+#include "Miscellaneous Code.h"
+#include "StopDPadOnSSS (except Wiimote).h"
+#include "DrawDI.h"
+#include "Code Menu.h"
+#include "IASA Overlay.h"
+#include "Control Codes.h"
+#include "Last Character Auto Select.h"
+#include "Tag Based Costumes.h"
+#include "Light Shield.h"
+#include "IkeClimbers.h"
+#include "AIDisplay.h"
+#include "C++Injection.h"
+//#include "FPS Display.h"
+using namespace std;
+
+
+
+int main()
+{
+	string OutputTextPath = "G:\\Compile\\ASM.txt";
+
+	CodeStart(OutputTextPath);
+	//place all ASM code here
+
+	//ReplayFix();
+
+	//NameIsFound();
+
+	//MenuControlCodes();
+
+	//StopStartAltFunctions();
+
+	//StopPokemonTrainerSwitch();
+
+	//StopDPadOnSSS();
+
+	//ConvertButtons();
+
+	//ItemSpawnControl();
+
+	//ClearASLData();
+
+	//SetTeamAttackTraining();
+
+	//LXPGreenOverlayFix();
+
+	CodeMenu(); tagBasedCostumes();
+
+	//musicPercentCode();
+
+	//DoubleFighterTest();
+
+	//UCF();
+
+	//CStickSlowFix();
+
+	//FixStickyRAlts();
+
+	//SelectLastCharacter();
+
+	//FixTr4shTeamToggle();
+
+	//cstickTiltTest();
+
+	//FPSDisplay();
+
+	//CStickTiltFix();
+
+	//DBZModeTest();
+
+	//slipperyTechs();
+
+	//lightShield();
+
+	//IkeClimbers();
+
+	//fixStanimaTextBug();
+
+	//AIDisplay();
+
+	//loadCppCodes(); writeInjectionsRepeat();
+
+	CodeEnd();
+}
