c9e22bdabe2289527c7e0b016c3445633c752e1d
Casino_Wolf <snowboardsilver7@gmail.com>
2021-07-20 00:06:00 -0700

    Update paths, add other alt stage buttons to random

--- PowerPC Assembly Functions/MainCode.cpp
+++ PowerPC Assembly Functions/MainCode.cpp
@@ -18,11 +18,44 @@
 //#include "FPS Display.h"
 using namespace std;
 
+bool MakeASM(string TextFilePath, string OutputAsmPath);
+bool MakeASM(string TextFilePath, string OutputAsmPath)
+{
+	string codeString;
+
+	ifstream textFile(TextFilePath);
+	if (textFile.is_open())
+	{
+		getline(textFile, codeString);
+	}
+
+	else {
+		cout << "Error: Unable to open txt file";
+		return false;
+	}
+
+	ofstream asmFile(OutputAsmPath);
+	if (asmFile.is_open())
+	{
+		for (int i = 0; i < codeString.length(); i += 16) {
+			asmFile << "* " << codeString.substr(i + 0, 8) << " " << codeString.substr(i + 8, 8) << "\n";
+		}
+
+		asmFile.close();
+	}
+
+	else {
+		cout << "Unable to open ASM file, check the OutputAsmPath";
+		return false;
+	}
+}
+
 
 
 int main()
 {
-	string OutputTextPath = "G:\\Compile\\ASM.txt";
+	string OutputTextPath = "C:\\Users\\AZPM\\Downloads\\DesiacXs_Code_Menu\\PowerPC-Assembly-Functions-master\\output\\asm.txt";
+	string OutputAsmPath = "C:\\Users\\AZPM\\Downloads\\DesiacXs_Code_Menu\\PowerPC-Assembly-Functions-master\\output\\CodeMenu.asm";
 
 	CodeStart(OutputTextPath);
 	//place all ASM code here
@@ -86,4 +119,6 @@ int main()
 	//loadCppCodes(); writeInjectionsRepeat();
 
 	CodeEnd();
+
+	MakeASM(OutputTextPath, OutputAsmPath);
 }
